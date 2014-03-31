882da41bdd8cc688103ddba2c9cdc05b1373cbc9
root <root@pisomdigital.com>
2014-03-31 20:35:36 +0000

    Tema

--- header.php
+++ header.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * The Header for our theme
- *
- * Displays all of the <head> section and everything up till <div id="main">
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
+
  */
 ?><!DOCTYPE html>
 <!--[if IE 7]>
@@ -18,6 +12,106 @@
 <!--[if !(IE 7) | !(IE 8) ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+	<title>Q Dica <?= $titlepage;?></title>
+	<link rel="shortcut icon" href="favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/tema.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/load.css" />
+
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/jquery.animate-colors-min.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/functions.js"></script>
+</head>
+
+<body <?php body_class(); ?>>
+
+	<!-- Top -->
+	<div id="top">
+
+		<!-- Logo -->
+		<div id="logo">
+			<a href="home" target="_top" border="0">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="logo-qdica" border="0" />
+			</a>
+		</div>
+		<!-- End Logo -->
+		
+		<!-- Menu -->
+		<div id="menu">
+			<!-- Menu 1 [Left side] -->
+			<div id="menu1">
+				<a href="home" name="home" class="menubt">Home</a>		
+				<a href="#" class="menubt">Servi&ccedil;os</a>
+				<a href="#" class="menubt">Dicas</a>
+				<a href="cadastro" name="cadastro" class="menubt">Cadastro</a>
+			</div>
+			<!-- End Menu 1 -->
+			
+			<!-- Menu 2 [Right side] -->
+			<div id="menu2">
+				<div class="menubtl" title="Clique para modificar localização" onClick="loadModal('localidade.php','500','300');"><span>Estou em:</span> <br />Mesquita, RJ</div>
+				<div class="menubt" name="departamentos" onClick="toggleDepartamentos()">Departamentos</div>
+				<div class="menubt">Login</div>
+			</div>
+			<!-- End Menu 2 -->
+			
+
+
+		</div>
+		<!-- End Menu -->
+
+	</div>
+	<!-- End Top -->
+
+	<div id="topline">
+
+		<div id="departamentos">
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/automoveis.png" />
+				<div class="nome">Automóveis</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/informatica.png" />
+				<div class="nome">Informática</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/mercado.png" />
+				<div class="nome">Mercado</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/telefonia.png" />
+				<div class="nome">Telefonia</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/roupas.png" />
+				<div class="nome">Roupas</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/viagens.png" />
+				<div class="nome">Viagens</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/jogos.png" />
+				<div class="nome">Jogos</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/downloads.png" />
+				<div class="nome">Downloads</div>
+			</div>
+			<div class="departamento">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/departamentos/eletronicos.png" />
+				<div class="nome">Eletrônicos</div>
+			</div>
+		</div>
+
+
+
+<?php 
+
+/* Codigo para exemplo
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
@@ -29,6 +123,7 @@
 	<![endif]-->
 	<?php wp_head(); ?>
 </head>
+
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
@@ -63,3 +158,5 @@
 	</header><!-- #masthead -->
 
 	<div id="main" class="site-main">
+*/
+?>
--- js/functions.js
+++ js/functions.js
@@ -1,128 +1,72 @@
-/**
- * Theme functions file
- *
- * Contains handlers for navigation, accessibility, header sizing
- * footer widgets and Featured Content slider
- *
- */
-( function( $ ) {
-	var body    = $( 'body' ),
-		_window = $( window );
+//////// GERAL /////////
 
-	// Enable menu toggle for small screens.
-	( function() {
-		var nav = $( '#primary-navigation' ), button, menu;
-		if ( ! nav ) {
-			return;
-		}
+function showDIV(id){ $(id).fadeIn(500); }
+function hideDIV(id){ $(id).fadeOut(500); }
 
-		button = nav.find( '.menu-toggle' );
-		if ( ! button ) {
-			return;
-		}
+$(document).ready(function(e) {
+ 
+ $.ajaxSetup({
+    'beforeSend' : function(xhr) {
+        xhr.overrideMimeType('text/html; charset=ISO-8859-1');
+    },
+});   
+	
+});
 
-		// Hide button if menu is missing or empty.
-		menu = nav.find( '.nav-menu' );
-		if ( ! menu || ! menu.children().length ) {
-			button.hide();
-			return;
-		}
 
-		$( '.menu-toggle' ).on( 'click.twentyfourteen', function() {
-			nav.toggleClass( 'toggled-on' );
-		} );
-	} )();
+// Load Modal
 
-	/*
-	 * Makes "skip to content" link work correctly in IE9 and Chrome for better
-	 * accessibility.
-	 *
-	 * @link http://www.nczonline.net/blog/2013/01/15/fixing-skip-to-content-links/
-	 */
-	_window.on( 'hashchange.twentyfourteen', function() {
-		var element = document.getElementById( location.hash.substring( 1 ) );
+function loadModal(url,w,h){
+	
+$("body").append('<div id="blackbg" onClick="hideDIV('+"'#blackbg'"+');hideDIV('+"'#loader'"+');"></div>');	
+$("body").append('<div id="loader"><div class="close" onClick="hideDIV('+"'#blackbg'"+');hideDIV('+"'#loader'"+');">X</div><div id="loadContent"></div></div>');	
 
-		if ( element ) {
-			if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) ) {
-				element.tabIndex = -1;
-			}
+mt = (h/2);
+ml = (w/2);
 
-			element.focus();
 
-			// Repositions the window on jump-to-anchor to account for header height.
-			window.scrollBy( 0, -80 );
-		}
-	} );
+showDIV('#blackbg');
 
-	$( function() {
-		// Search toggle.
-		$( '.search-toggle' ).on( 'click.twentyfourteen', function( event ) {
-			var that    = $( this ),
-				wrapper = $( '.search-box-wrapper' );
+$('#loader').fadeIn(500, function(){
+$('#loader').animate({width:w+"px",height:h+"px",marginTop:"-"+mt+"px",marginLeft:"-"+ml+"px",opacity:"1"},500);
+		
+	});
 
-			that.toggleClass( 'active' );
-			wrapper.toggleClass( 'hide' );
+$('#loadContent').load("load/"+url);
+	
+	
+}
 
-			if ( that.is( '.active' ) || $( '.search-toggle .screen-reader-text' )[0] === event.target ) {
-				wrapper.find( '.search-field' ).focus();
-			}
-		} );
+//Função para visualizar departamentos
 
-		/*
-		 * Fixed header for large screen.
-		 * If the header becomes more than 48px tall, unfix the header.
-		 *
-		 * The callback on the scroll event is only added if there is a header
-		 * image and we are not on mobile.
-		 */
-		if ( _window.width() > 781 ) {
-			var mastheadHeight = $( '#masthead' ).height(),
-				toolbarOffset, mastheadOffset;
+function toggleDepartamentos(){
+	
+		if($("#departamentos").is(":visible")){
+			
+	$("div[name='departamentos']").removeClass("selected");
+			$("#departamentos").fadeOut(500, function(){
 
-			if ( mastheadHeight > 48 ) {
-				body.removeClass( 'masthead-fixed' );
-			}
+	$("#topline").animate({height:'6px'},500);
+		
+		
+		});
+		
+		
+		} else{
+	
+	$("div[name='departamentos']").addClass("selected");
+	$("#topline").animate({height:'105px'},500, function(){
+		
+		$("#departamentos").fadeIn(500);
+		
+		});
+	
+	
+	}}
+	
 
-			if ( body.is( '.header-image' ) ) {
-				toolbarOffset  = body.is( '.admin-bar' ) ? $( '#wpadminbar' ).height() : 0;
-				mastheadOffset = $( '#masthead' ).offset().top - toolbarOffset;
 
-				_window.on( 'scroll.twentyfourteen', function() {
-					if ( ( window.scrollY > mastheadOffset ) && ( mastheadHeight < 49 ) ) {
-						body.addClass( 'masthead-fixed' );
-					} else {
-						body.removeClass( 'masthead-fixed' );
-					}
-				} );
-			}
-		}
 
-		// Focus styles for menus.
-		$( '.primary-navigation, .secondary-navigation' ).find( 'a' ).on( 'focus.twentyfourteen blur.twentyfourteen', function() {
-			$( this ).parents().toggleClass( 'focus' );
-		} );
-	} );
 
-	// Arrange footer widgets vertically.
-	if ( $.isFunction( $.fn.masonry ) ) {
-		$( '#footer-sidebar' ).masonry( {
-			itemSelector: '.widget',
-			columnWidth: function( containerWidth ) {
-				return containerWidth / 4;
-			},
-			gutterWidth: 0,
-			isResizable: true,
-			isRTL: $( 'body' ).is( '.rtl' )
-		} );
-	}
 
-	// Initialize Featured Content slider.
-	_window.load( function() {
-		if ( body.is( '.slider' ) ) {
-			$( '.featured-content' ).featuredslider( {
-				selector: '.featured-content-inner > article',
-				controlsContainer: '.featured-content'
-			} );
-		}
-	} );
-} )( jQuery );
+
