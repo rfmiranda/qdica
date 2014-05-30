b29bb191320e9e507d977b866459da5582b1ad44
raphaelfreitas <raphaelfreitas@trii.com.br>
2014-05-30 10:03:43 -0300

    Plugin slider

--- wp-content/themes/qdica/front-page.php
+++ wp-content/themes/qdica/front-page.php
@@ -2,9 +2,5 @@
 get_header();
 ?>
 
-<article class='row'>
-	<section class='col-lg-12 col-md-12 col-sm-12 col-xs-12'>
-		teste
-	</section>
-</article>
+ <?php putRevSlider( "banner_principal" ); ?>
 <?php get_footer();
