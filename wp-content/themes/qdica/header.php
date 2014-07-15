562cc2a9c3f75a90522612dec7aa61de916a2262
raphaelfreitas <rfn_vip@hotmail.com>
2014-07-14 23:18:51 -0300

    Login display name

--- wp-content/themes/qdica/header.php
+++ wp-content/themes/qdica/header.php
@@ -7,6 +7,8 @@
  * @package Odin
  * @since 2.2.0
  */
+
+global $current_user;
 ?><!DOCTYPE html>
 <!--[if IE 7]>
 <html class="no-js ie ie7 lt-ie9 lt-ie8" <?php language_attributes(); ?>>
@@ -49,41 +51,44 @@
 		</div>
 		<!-- Menu -->
 		<div class="container">
-		<div id="menu" class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-			<!-- Menu 1 [Left side] -->
+			<div id="menu" class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				<!-- Menu 1 [Left side] -->
+				
+				<?php wp_nav_menu( array('menu' => 'Menu1', 'menu_class'  => 'menu-principal') ); ?>
+				
+			</div>
+			<!-- Fim Menu 1 -->
+			<!-- Menu Responsivo -->
 			
-			<?php wp_nav_menu( array('menu' => 'Menu1', 'menu_class'  => 'menu-principal') ); ?>
-			
-		</div>
-		<!-- Fim Menu 1 -->
-		<!-- Menu Responsivo -->
-		
-		<!-- Fim Menu Responsivo -->
-		<!-- Logo -->
-		<div id="logo" class="col-lg-4 col-md-4 col-sm-8 col-xs-8 text-center">
-			<a href="home" target="_top" border="0">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" class="img-thumbnail img-responsive" alt="logo-qdica" border="0" />
-			</a>
-		</div>
-		<!-- Fim Logo -->
-		<!-- Menu 2 [Right side] -->
-		<div class="menu-menu2-container col-lg-4 col-md-4 col-sm-5 col-xs-5">
-			<ul class="menu">
-				<li class="menubtl" title="Clique para modificar localização" onClick="loadModal('localidade.php','500','300');"><span>Estou em:</span> <br />Rio de Janeiro, RJ</li>
-				<li class="menu-item" name="departamentos" onClick="toggleDepartamentos()"><a href="#">Departamentos</a></li>
-				<li class="menu-item"><a href="#">Login</a></li>
-			</ul>
-		</div><!-- Fim Menu 2 -->
+			<!-- Fim Menu Responsivo -->
+			<!-- Logo -->
+			<div id="logo" class="col-lg-4 col-md-4 col-sm-8 col-xs-8 text-center">
+				<a href="home" target="_top" border="0">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/logo.png" class="img-thumbnail img-responsive" alt="logo-qdica" border="0" />
+				</a>
+			</div>
+			<!-- Fim Logo -->
+			<!-- Menu 2 [Right side] -->
+			<div class="menu-menu2-container col-lg-4 col-md-4 col-sm-5 col-xs-5">
+				<ul class="menu">
+					<li class="menubtl" title="Clique para modificar localização" onClick="loadModal('localidade.php','500','300');"><span>Estou em:</span> <br />Rio de Janeiro, RJ</li>
+					<li class="menu-item" name="departamentos" onClick="toggleDepartamentos()"><a href="#">Departamentos</a></li>
+					<li class="menu-item">
+						<a href="#">
+						<?php if ( is_user_logged_in() ) { echo $current_user->display_name.' <i class="fa fa-sort-desc"></i>'; } else { echo 'Login'; } ?>
+						</a>
+					</li>
+				</ul>
+			</div><!-- Fim Menu 2 -->
+		</div>]
 		<!-- Login Responsivo -->
-		<div class="menu-responsivo col-xs-2">
-			<div class="menu-bt">
-				Login
+		<div class="menu-responsivo">
+			<div class="menu-bt-login">
+				<?php if ( is_user_logged_in() ) { echo $current_user->display_name.' <i class="fa fa-sort-desc"></i>'; } else { echo 'Login'; } ?>
 			</div>
 			<?php //wp_nav_menu( array('menu' => 'Menu1', 'menu_class'  => 'menu-principal') ); ?>
 		</div>
 		<!-- Fim Login Responsivo -->
-		</div>
-
 	</div><!-- Fim Top -->
 
 	
@@ -132,16 +137,20 @@
 	</div>
 	<!-- Fim Top -->
 
+	<?php if ( !is_user_logged_in() ) { ?>
 	<div class="login-container container">
 		<div class="login-panel container">
-			<input type="text" class="form-control m-b-small" placeholder="Login ou Email" name="login" />
-			<input type="password" class="form-control m-b-small" placeholder="Senha" name="senha" />
-			<div class="m-b-small text-default">
-				<input type="checkbox" name="manter" class="m-r-small" /> Manter-me conectado
-			</div>
-			<input type="submit" class="btn btn-primary m-t" value="Entrar"/>
+			<form name="login" action="<?php echo home_url('/wp-login.php'); ?>" method="post">
+				<input type="text" class="form-control m-b-small" placeholder="Login ou Email" name="log" />
+				<input type="password" class="form-control m-b-small" placeholder="Senha" name="pwd" />
+				<div class="m-b-small text-default">
+					<input type="checkbox" name="rememberme" class="m-r-small" /> Manter-me conectado
+				</div>
+				<input type="submit" class="btn btn-primary m-t" value="Entrar"/>
+			</form>
 		</div>
 	</div>
+	<?php } ?>
 
 
 	<div class="container">
